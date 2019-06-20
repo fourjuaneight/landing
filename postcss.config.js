111be6a7224ddb7be99f81ef5bc83fb9f717b646
Juan Villela <juan@villela.co>
2019-06-20 17:32:28 -0400

    Config update
    Removed local setup files from repo.
    Removed Webpack dependency.
    Updated Gulp config for Hugo fix and PurgeCSS only (more info in CSS update commit).
    Updated PostCSS for simpler setup.
    Updated Hugo config.
    Added Mergify config.

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,17 @@
 // Learn more about PostCSS:
 // https://github.com/postcss/postcss
 
-module.exports = function(ctx = {}) {
-  const { file } = ctx;
-  const opts = ctx.options || {};
-  const isProduction =
-    opts.env === 'production' || process.env.NODE_ENV === 'production';
-
+module.exports = () => {
   return {
-    parser: 'postcss-scss',
     plugins: {
       'autoprefixer': { browsers: ['> 5%'] },
-      'cssnano': isProduction ? { autoprefixer: true } : false,
-      'laggard': {},
-      'postcss-browser-reporter': isProduction ? {} : false,
+      'cssnano': { preset: 'default' },
       'postcss-calc': {},
       'postcss-custom-properties': {},
       'postcss-import': {},
       'postcss-mixins': {},
       'postcss-nested': {},
-      'postcss-reporter': {},
-      'postcss-simple-vars': {}
-    }
+      'postcss-simple-vars': {},
+    },
   };
 };
