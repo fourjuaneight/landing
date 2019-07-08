bcbdcc7b219910a96ca0cfcd9a17d45452649a18
Juan Villela <juan@villela.co>
2019-07-08 16:29:43 -0400

    Changed theme logical operators to switch statements for better performance.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,14 +2,20 @@ const toggle = document.querySelector('#theme');
 const root = document.querySelector('html');
 
 toggle.addEventListener('input', () => {
-  if (toggle.checked === false) {
-    localStorage.setItem('theme', 'light');
-  } else {
-    localStorage.setItem('theme', 'dark');
+  switch (toggle.checked) {
+    case false:
+      localStorage.setItem('theme', 'light');
+      break;
+    case true:
+      localStorage.setItem('theme', 'dark');
+      break;
   }
-  if (root.getAttribute('data-theme') === 'dark') {
-    root.setAttribute('data-theme', 'light');
-  } else {
-    root.setAttribute('data-theme', 'dark');
+  switch (root.getAttribute('data-theme')) {
+    case 'dark':
+      root.setAttribute('data-theme', 'light');
+      break;
+    case 'light':
+      root.setAttribute('data-theme', 'dark');
+      break;
   }
 });
