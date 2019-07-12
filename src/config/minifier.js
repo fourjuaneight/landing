6f55113bfca93d91158950255a8fc7723a823e74
Juan Villela <juan@villela.co>
2019-07-12 00:51:56 -0400

    Added Markdown engine, content minifier, and Tags configs.

---
+++ src/config/minifier.js
@@ -0,0 +1,16 @@
+const htmlmin = require('html-minifier');
+
+const htmlMinTransform = (value, outputPath) => {
+  if (outputPath.indexOf('.html') > -1) {
+    let minified = htmlmin.minify(value, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyCSS: true
+    });
+    return minified;
+  }
+  return value;
+}
+
+module.exports = htmlMinTransform;
