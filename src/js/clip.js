9f2eb3b6e1affd976bcf42dedc01bb23fa202475
Juan Villela <juan@villela.co>
2019-07-12 00:55:11 -0400

    Assets updates
    Moved CSS and JS files.
    Removed SW script. Will be Webpack generated.

--- src/js/clip.js
+++ src/js/clip.js
@@ -1,4 +1,5 @@
 const label = document.querySelectorAll('button.clip');
+
 label.forEach(copy => {
   copy.addEventListener('click', () => {
     const selection = window.getSelection();
@@ -6,20 +7,24 @@ label.forEach(copy => {
     const pre = copy.nextSibling;
     const code = pre.querySelector('code');
     const original = copy.innerHTML;
+
     range.selectNodeContents(code);
     selection.removeAllRanges();
     selection.addRange(range);
+
     try {
       document.execCommand('copy');
       selection.removeAllRanges();
       copy.classList.toggle('f7');
       copy.innerHTML = 'Copied!';
+
       setTimeout(() => {
         copy.classList.toggle('f7');
         copy.innerHTML = original;
       }, 1000);
     } catch {
       copy.innerHTML = "Can't copy, hit Ctrl+C!";
+
       setTimeout(() => {
         copy.innerHTML = original;
       }, 1000);
