bdbfb1b6c622e2d897b3641bf795f5f6555e511a
Juan Villela <juan@villela.co>
2019-07-29 20:06:53 -0400

    Simplified critical CSS and font loading JS components pipeline.

--- src/config/minifier.js
+++ src/config/minifier.js
@@ -2,15 +2,16 @@ const htmlmin = require('html-minifier');
 
 const htmlMinTransform = (value, outputPath) => {
   if (outputPath.indexOf('.html') > -1) {
-    let minified = htmlmin.minify(value, {
-      useShortDoctype: true,
-      removeComments: true,
+    const minified = htmlmin.minify(value, {
       collapseWhitespace: true,
       minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      useShortDoctype: true,
     });
     return minified;
   }
   return value;
-}
+};
 
 module.exports = htmlMinTransform;
