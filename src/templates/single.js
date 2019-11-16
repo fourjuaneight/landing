5903f52dc2a47ad02d69497c064c9b7b90d0e0ee
Juan Villela <juan@villela.co>
2019-11-16 15:01:16 -0500

    Added posts single template.

---
+++ src/templates/single.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
+import cx from 'classnames';
+
+import styles from '../styles/styles.module.scss';
+
+import CleanHTML from '../components/cleanHTML';
+import Layout from '../components/layout';
+
+const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: {
+      slug: {
+        eq: $slug
+      }
+    }) {
+      frontmatter {
+        date
+        tag
+        title
+      }
+      html
+    }
+  }
+  }
+`;
+
+const Single = ({ data, location }) => {
+  const {
+    markdownRemark: {
+      frontmatter: { date, tag, title },
+      html,
+    },
+  } = data;
+  const raw = new Date(date);
+  const iso = raw.toISOString().substring(0, 10);
+
+  return (
+    <Layout pageTitle={title} location={location}>
+      <article
+        className={cx(
+          styles.flex,
+          styles.flexColumn,
+          styles.itemsFlexStart,
+          styles.m_horizontal,
+          styles.w100
+        )}
+      >
+        <h1
+          className={cx(
+            styles.mra,
+            styles.relative,
+            styles.marked,
+            styles.title
+          )}
+        >
+          {title}
+        </h1>
+        <section>
+          <time
+            dateTime={iso}
+            className={cx(styles.f7, styles.mra, styles.meta)}
+          >
+            <span aria-label="Posted on">&tau;</span>
+            {date}
+          </time>
+          <p className={cx(styles.f7, styles.ma0, styles.mr1)}>
+            <span aria-hidden="true" className={styles.meta}>
+              #
+            </span>
+            <Link to={`/tags/${tag}`} itemprop="about" className={styles.meta}>
+              {tag}
+            </Link>
+          </p>
+        </section>
+        <section>
+          <CleanHTML html={html} />
+        </section>
+      </article>
+    </Layout>
+  );
+};
+
+Single.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object.isRequired,
+  }),
+  location: PropTypes.object.isRequired,
+};
+
+export { query, Single as default };
