a94320fd70032d83d60e09a69d80b28710d539eb
Juan Villela <juan@villela.co>
2019-11-16 19:34:56 -0500

    Broke out article element to component for single template.

---
+++ src/components/post.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import cx from 'classnames';
+
+import styles from '../styles/styles.module.scss';
+
+import CleanHTML from './cleanHTML';
+
+const Post = ({ date, html, index, list, slug, tag, title }) => (
+  <article
+    key={list && index}
+    className={cx(
+      styles.flex,
+      styles.flexColumn,
+      styles.itemsFlexStart,
+      styles.m_horizontal,
+      styles.w100
+    )}
+  >
+    {list ? (
+      <Link to={`/posts${slug}`} className={styles.permalink}>
+        <h2 className={cx(styles.ma0, styles.list_title)}>{title}</h2>
+      </Link>
+    ) : (
+      <h1
+        className={cx(styles.mra, styles.relative, styles.marked, styles.title)}
+      >
+        {title}
+      </h1>
+    )}
+    <section
+      className={cx(
+        styles.contentBetween,
+        styles.flex,
+        styles.flexWrap,
+        styles.meta_wrap,
+        styles.w100
+      )}
+    >
+      <time dateTime={date} className={cx(styles.f7, styles.mra, styles.meta)}>
+        <span aria-label="Posted on">&tau;</span>
+        {date}
+      </time>
+      <p className={cx(styles.f7, styles.ma0, styles.mr1)}>
+        <span aria-hidden="true" className={styles.meta}>
+          #
+        </span>
+        <Link to={`/tags/${tag}/`} itemProp="about" className={styles.meta}>
+          {tag}
+        </Link>
+      </p>
+    </section>
+    <CleanHTML html={html} />
+  </article>
+);
+
+Post.propTypes = {
+  date: PropTypes.string.isRequired,
+  html: PropTypes.string.isRequired,
+  index: PropTypes.string,
+  list: PropTypes.bool,
+  slug: PropTypes.string,
+  tag: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Post;
--- src/templates/single.js
+++ src/templates/single.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
-import cx from 'classnames';
+import { graphql } from 'gatsby';
 
-import styles from '../styles/styles.module.scss';
-
-import CleanHTML from '../components/cleanHTML';
 import Layout from '../components/layout';
+import Post from '../components/post';
 
 export const query = graphql`
-  query PostQuery($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+  query SingleQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       excerpt(format: PLAIN, truncate: false)
       frontmatter {
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "YYYY-MM-DD")
         tag
         title
       }
@@ -30,51 +27,10 @@ const Single = ({ data, location }) => {
       html,
     },
   } = data;
-  const raw = new Date(date);
-  const iso = raw.toISOString().substring(0, 10);
 
   return (
     <Layout pageDescription={excerpt} pageTitle={title} location={location}>
-      <article
-        className={cx(
-          styles.flex,
-          styles.flexColumn,
-          styles.itemsFlexStart,
-          styles.m_horizontal,
-          styles.w100
-        )}
-      >
-        <h1
-          className={cx(
-            styles.mra,
-            styles.relative,
-            styles.marked,
-            styles.title
-          )}
-        >
-          {title}
-        </h1>
-        <section>
-          <time
-            dateTime={iso}
-            className={cx(styles.f7, styles.mra, styles.meta)}
-          >
-            <span aria-label="Posted on">&tau;</span>
-            {date}
-          </time>
-          <p className={cx(styles.f7, styles.ma0, styles.mr1)}>
-            <span aria-hidden="true" className={styles.meta}>
-              #
-            </span>
-            <Link to={`/tags/${tag}`} itemprop="about" className={styles.meta}>
-              {tag}
-            </Link>
-          </p>
-        </section>
-        <section>
-          <CleanHTML html={html} />
-        </section>
-      </article>
+      <Post date={date} html={html} tag={tag} title={title} />
     </Layout>
   );
 };
