c88e88c1cb21141fcb68b906999c033f6fa4eac1
Juan Villela <juan@villela.co>
2019-12-15 18:02:51 -0500

    Better font loading
    Added FontFace API based font loading using subset on initial load, then swap with full version.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,29 +1,19 @@
-const { readdirSync } = require('fs');
 const React = require('react');
+const globby = require('globby').sync;
 
-const publicFolder = './public';
-const isWorker = file => file.includes('ww') && file.endsWith('.worker.js');
-const shouldPreload = (file, preloads) =>
-  file.startsWith('ww') && preloads.some(preload => file.includes(preload));
+exports.onRenderBody = ({ pathPrefix = '', setHeadComponents }) => {
+  const files = globby('./public/fonts/*-sub.{woff2|woff}');
 
-let preloadScripts = [];
-
-exports.onRenderBody = ({ setHeadComponents }, { preloads = [] } = {}) => {
-  if (!preloads.length) return;
-
-  if (!preloadScripts.length) {
-    preloadScripts = readdirSync(publicFolder)
-      .filter(file => isWorker(file) && shouldPreload(file, preloads))
-      .map(file => (
-        <link
-          key={file}
-          as="worker"
-          crossOrigin="anonymous"
-          href={`/${file}`}
-          rel="preload"
-        />
-      ));
-  }
-
-  setHeadComponents(preloadScripts);
+  setHeadComponents(
+    files.map((file, key) => (
+      <link
+        key={key}
+        rel="preload"
+        as="font"
+        type={`font/${file.match(/woff2|woff/g)}`}
+        crossOrigin="anonymous"
+        href={file.replace('./public', pathPrefix)}
+      />
+    ))
+  );
 };
--- src/components/seo.js
+++ src/components/seo.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const SEO = ({ pageDescription, pageTitle, postPublishDate, location }) => {
-  const { fc, icon, meta, rb, rr, spt } = useStaticQuery(graphql`
+  const { icon, meta, spt } = useStaticQuery(graphql`
     query HelmetQuery {
       meta: site {
         siteMetadata {
@@ -23,45 +23,6 @@ const SEO = ({ pageDescription, pageTitle, postPublishDate, location }) => {
       spt: file(dir: { regex: "/images/" }, name: { regex: "/safari/" }) {
         publicURL
       }
-      rr: allFile(
-        filter: {
-          dir: { regex: "/fonts/" }
-          name: { regex: "/(?!.*sub)^Rubik-Regular.*$/" }
-        }
-        sort: { fields: ext, order: DESC }
-      ) {
-        edges {
-          node {
-            publicURL
-            extension
-          }
-        }
-      }
-      rb: allFile(
-        filter: {
-          dir: { regex: "/fonts/" }
-          name: { regex: "/(?!.*sub)^Rubik-Bold.*$/" }
-        }
-        sort: { fields: ext, order: DESC }
-      ) {
-        edges {
-          node {
-            publicURL
-            extension
-          }
-        }
-      }
-      fc: allFile(
-        filter: { dir: { regex: "/fonts/" }, name: { regex: "/FiraCode/" } }
-        sort: { fields: ext, order: DESC }
-      ) {
-        edges {
-          node {
-            publicURL
-            extension
-          }
-        }
-      }
     }
   `);
 
@@ -70,11 +31,12 @@ const SEO = ({ pageDescription, pageTitle, postPublishDate, location }) => {
   } = meta;
   const dynamicTitle = pageTitle ? `${pageTitle} | ${title}` : title;
   const dynamicDesc = pageDescription || description;
+  const baseURL = typeof location.origin !== 'undefined' ? location.origin : '';
   let schemaOrgJSONLD = {
     '@context': 'http://schema.org',
     '@type': 'WebSite',
     name: dynamicTitle,
-    url: `${location.origin}/`,
+    url: `${typeof location.origin !== 'undefined' ? location.origin : '/'}`,
   };
   /* eslint-disable sort-keys */
   if (location.pathname.match(/posts\/.*\//)) {
@@ -83,10 +45,10 @@ const SEO = ({ pageDescription, pageTitle, postPublishDate, location }) => {
       '@type': 'Article',
       mainEntityOfPage: {
         '@type': 'WebPage',
-        '@id': `${location.origin}${location.pathname}`,
+        '@id': `${baseURL}${location.pathname}`,
       },
       headline: pageTitle,
-      image: `${location.origin}${icon.fixed.src}`,
+      image: `${baseURL}${icon.fixed.src}`,
       author: {
         '@type': 'Person',
         name: author,
@@ -96,7 +58,7 @@ const SEO = ({ pageDescription, pageTitle, postPublishDate, location }) => {
         name: author,
         logo: {
           '@type': 'ImageObject',
-          url: `${location.origin}${icon.fixed.src}`,
+          url: `${baseURL}${icon.fixed.src}`,
         },
       },
       datePublished: postPublishDate,
@@ -104,40 +66,6 @@ const SEO = ({ pageDescription, pageTitle, postPublishDate, location }) => {
     };
   }
   /* eslint-enable */
-  const fontFace = `
-    @font-face {
-      font-family: Rubik;
-      src:
-        ${rr.edges.map(
-          ({ node }) => `url(${node.publicURL}) format('${node.extension}')`
-        )};
-      font-weight: 400;
-      font-style: normal;
-      font-display: swap;
-    }
-    
-    @font-face {
-      font-family: Rubik;
-      src:
-        ${rb.edges.map(
-          ({ node }) => `url(${node.publicURL}) format('${node.extension}')`
-        )};
-      font-weight: 700;
-      font-style: normal;
-      font-display: swap;
-    }
-    
-    @font-face {
-      font-family: Fira Code;
-      src:
-        ${fc.edges.map(
-          ({ node }) => `url(${node.publicURL}) format('${node.extension}')`
-        )};
-      font-weight: 400;
-      font-style: normal;
-      font-display: swap;
-    }
-  `;
 
   return (
     <Helmet defaultTitle={dynamicTitle}>
@@ -145,45 +73,31 @@ const SEO = ({ pageDescription, pageTitle, postPublishDate, location }) => {
         name="viewport"
         content="width=device-width,minimum-scale=1.0,initial-scale=1.0,maximum-scale=5.0,viewport-fit=cover"
       />
-      <link rel="canonical" href={`${location.origin}${location.pathname}`} />
+      <link rel="canonical" href={`${baseURL}${location.pathname}`} />
       <meta property="author" content={author} />
       <meta name="description" content={dynamicDesc} />
-      <meta name="image" content={`${location.origin}${icon.fixed.src}`} />
+      <meta name="image" content={`${baseURL}${icon.fixed.src}`} />
       <script type="application/ld+json">
         {JSON.stringify(schemaOrgJSONLD)}
       </script>
       <meta property="og:description" content={dynamicDesc} />
-      <meta
-        property="og:image"
-        content={`${location.origin}${icon.fixed.src}`}
-      />
+      <meta property="og:image" content={`${baseURL}${icon.fixed.src}`} />
       <meta property="og:site_name" content={title} />
       <meta property="og:title" content={dynamicTitle} />
       <meta
         property="og:type"
         content={location.pathname.match(/posts\/.*\//) ? 'article' : 'website'}
       />
-      <meta
-        property="og:url"
-        content={`${location.origin}${location.pathname}`}
-      />
+      <meta property="og:url" content={`${baseURL}${location.pathname}`} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:description" content={dynamicDesc} />
-      <meta
-        name="twitter:image"
-        content={`${location.origin}${icon.fixed.src}`}
-      />
+      <meta name="twitter:image" content={`${baseURL}${icon.fixed.src}`} />
       <meta name="twitter:title" content={dynamicTitle} />
       <meta property="twitter:site" content={social} />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       <meta name="apple-mobile-web-app-title" content={dynamicTitle} />
-      <link
-        rel="mask-icon"
-        color={theme}
-        href={`${location.origin}${spt.publicURL}`}
-      />
-      <style>{fontFace}</style>
+      <link rel="mask-icon" color={theme} href={`${baseURL}${spt.publicURL}`} />
     </Helmet>
   );
 };
