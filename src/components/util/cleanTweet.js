15f4759d534a5317de326a006b2b6db4ff8eb3d0
Juan Villela <juan@villela.co>
2020-02-26 00:47:25 -0500

    Added link conversion for inlined links.

--- src/components/util/cleanTweet.js
+++ src/components/util/cleanTweet.js
@@ -8,9 +8,13 @@ const CleanTweet = ({ string }) => {
     /(@)([a-zA-Z0-9_]+)/g,
     '<a href="https://twitter.com/$2" target="_blank" rel="nofollow noreferrer">$1$2</a>'
   );
+  const withLink = withAts.replace(
+    /(https:\/\/t.co\/[a-zA-z0-9]+)/g,
+    '<a href="$1" target="_blank" rel="nofollow noreferrer">$1</a>'
+  );
 
   // eslint-disable-next-line react/no-danger
-  return <p dangerouslySetInnerHTML={createMarkup(withAts)} />;
+  return <p dangerouslySetInnerHTML={createMarkup(withLink)} />;
 };
 
 CleanTweet.propTypes = {
