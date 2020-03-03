fd150efb842f45bf078211be8c9b1e8ff5e5367f
Juan Villela <juan@villela.co>
2020-03-03 01:07:22 -0500

    Updated cleanTweet component to catch expanded URLs.

--- src/components/util/cleanTweet.js
+++ src/components/util/cleanTweet.js
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types';
 const CleanTweet = ({ string }) => {
   const createMarkup = content => ({ __html: content });
   const unicode = string.replace(/U\+([a-zA-Z0-9]+)/g, '&#x$1;');
-  const withAts = unicode.replace(
+  const withLink = unicode.replace(
+    /(https?:\/\/?[\w/\-+:?=%.#]+\.[\w/\-+:?=%.#]+)/g,
+    '<a href="$1" target="_blank" rel="nofollow noreferrer">$1</a>'
+  );
+  const withAts = withLink.replace(
     /(@)([a-zA-Z0-9_]+)/g,
     '<a href="https://twitter.com/$2" target="_blank" rel="nofollow noreferrer">$1$2</a>'
   );
-  const withLink = withAts.replace(
-    /(https:\/\/t.co\/[a-zA-z0-9]+)/g,
-    '<a href="$1" target="_blank" rel="nofollow noreferrer">$1</a>'
-  );
 
   // eslint-disable-next-line react/no-danger
-  return <p dangerouslySetInnerHTML={createMarkup(withLink)} />;
+  return <p dangerouslySetInnerHTML={createMarkup(withAts)} />;
 };
 
 CleanTweet.propTypes = {
