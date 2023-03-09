6388595f5cab98eaa197d3e909b86fe2dbb2f0d3
Juan Villela <juan@villela.co>
2023-03-09 16:51:38 -0500

    chore: Use context url for redirect base.

--- functions/api/contact.ts
+++ functions/api/contact.ts
@@ -11,8 +11,8 @@ interface RequestParams {
 }
 
 export const onRequestPost = async (context: RequestParams) => {
-  const base = 'https://juanvillela.dev';
   const url = new URL(context.request.url);
+  const { origin } = url;
 
   try {
     const date = new Date();
@@ -20,18 +20,17 @@ export const onRequestPost = async (context: RequestParams) => {
     // get fom data
     const input = await context.request.formData();
     const data = Object.fromEntries(input.entries());
-    console.log(url);
     
     if (data) {
       await context.env.CONTACT.put(timestamp, JSON.stringify(data));
 
-      return Response.redirect(`${base}/contact/success`);
+      return Response.redirect(`${origin}/contact/success`);
     }
 
-    return Response.redirect(`${base}/contact/error`);
+    return Response.redirect(`${origin}/contact/error`);
   } catch (error) {
     console.log('contact', { error });
 
-    return Response.redirect(`${base}/contact/error`);
+    return Response.redirect(`${origin}/contact/error`);
   }
 };
